2af76247e0e94a6f493e6ba4a34cca3f05b60574
LE2D22yanoura <k023g0120@g.neec.ac.jp>
2024-05-31 14:09:42 +0900

    プロジェクト ファイルを追加します。

---
+++ Object3D.PS.hlsl
@@ -0,0 +1,19 @@
+
+struct Material
+{
+    float4 color;
+};
+
+ConstantBuffer<Material> gMaterial : register(b0);
+
+struct PixelShaderOutput
+{
+    float4 color : SV_TARGET0;
+};
+
+PixelShaderOutput main()
+{
+    PixelShaderOutput output;
+    output.color = gMaterial.color; //白
+    return output;
+}
